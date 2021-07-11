f5f0d0c230bfde62ed4ad646ed8467f476307148
Andrew <codesmith32@gmail.com>
2021-07-11 18:54:54 -0400

    Initial Game Setup
    - structure game classes
    - initial render implementation

--- src/gamecontroller.cpp
+++ src/gamecontroller.cpp
@@ -1,28 +1,59 @@
 #include "gamecontroller.h"
 
 
-GameController::GameController(olc::PixelGameEngine& pge): pge(pge) {
+game::GameController::GameController(olc::PixelGameEngine* pge, Multiplayer* mp, int width, int height):
+    pge(pge), mp(mp), width(width), height(height) {
 
+    GameObj::pge = &pge;
+    GameObj::ctrl = this;
+
+    screenW = pge.ScreenWidth();
+    screenH = pge.ScreenHeight();
+
+    cw = screenW / 2 / width;
+    ch = screenH / height;
+    hwidth = screenW / 2;
+
+    selected = nullptr;
 }
 
-GameController::~GameController() {}
+game::GameController::~GameController() {}
 
-void GameController::update(float delta) {
-    for(Ship& ship : ships){
-        drawShip(ship);
+void game::GameController::update(float delta) {
+    control();
+    render();
+}
+
+
+void game::GameController::render() {
+    pge->Clear(olc::BLACK);
+
+    pge->DrawLine(hwidth,0, hwidth,screenH, 0xFF808080);
+
+    for(int y=0; y<height; y++)
+    for(int x=0; x<width; x++) {
+        pge->Draw(x, y, olc::WHITE);
+        pge->Draw(x+hwidth, y, olc::WHITE);
+    }
+
+    for(auto& ship : ships) {
+        ship.draw();
+    }
+    for(auto& splash : splashes) {
+        splash.draw();
+    }
+    for(auto& hit : hits) {
+        hit.draw();
     }
 }
 
-
-void GameController::drawShip(GameController::Ship& ship) {
-    olc::Pixel col = olc::WHITE;
-    pge.Draw(ship.position, col);
-    if(ship.direction == Ship::Vertical){
-        pge.Draw(ship.position - olc::vi2d {0, 1}, col);
-        pge.Draw(ship.position + olc::vi2d {0, 1}, col);
-    }
-    if(ship.direction == Ship::Horizontal){
-        pge.Draw(ship.position - olc::vi2d {1, 0}, col);
-        pge.Draw(ship.position + olc::vi2d {1, 0}, col);
+void game::Ship::draw() {
+    for(int n=-1; n<=1; n++) {
+        pge->FillRect(
+            x + n * (dir == HOR) + 2,
+            y + n * (dir == VERT) + 2,
+            ctrl->cw - 4, ctrl->ch - 4,
+            olc::WHITE
+        );
     }
 }
--- src/gamecontroller.h
+++ src/gamecontroller.h
@@ -4,26 +4,64 @@
 #include <vector>
 #include <list>
 #include <iostream>
+#include "multiplayer.h"
 
+namespace game {
 
+    class GameController;
 
-class GameController {
-    olc::PixelGameEngine& pge;
-public:
+    class GameObj {
+    public:
+        static olc::PixelGameEngine* pge;
+        static GameController* ctrl;
 
-    struct Ship {
-        olc::vi2d position;
-        enum DIR {
-            Horizontal, Vertical
-        } direction;
+        olc::vi2d pos;
+        virtual void draw() = 0;
     };
 
-    std::list<Ship> ships;
+    class Ship: public GameObj {
+        enum DIR {
+            HOR, VERT,
+        } dir;
 
-    GameController(olc::PixelGameEngine& pge);
-    virtual ~GameController();
+        int health;
 
-    virtual void update(float delta);
-    virtual void drawShip(Ship& ship);
+        virtual void draw();
+    };
+    class Splash: public GameObj {
+        int t;
 
-};
+        virtual void draw();
+    };
+    class Hit: public GameObj {
+        int t;
+
+        virtual void draw();
+    };
+
+    class GameController {
+        olc::PixelGameEngine* pge;
+        Multiplayer* mp;
+
+    public:
+        int width, height;
+        int screenW, screenH;
+        int cw, ch;
+        int hwidth;
+        
+        Ship* selected;
+
+        GameController(olc::PixelGameEngine* pge, Multiplayer* mp, int width=20, int height=20);
+        virtual ~GameController();
+
+        virtual void update(float delta);
+
+        void control();
+        void render();
+
+        std::vector<Ship> ships;
+        std::vector<Splash> splashes;
+        std::vector<Hit> hits;
+    };
+
+}
